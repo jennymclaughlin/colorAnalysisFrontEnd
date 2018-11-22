d5077793b19c725fa953bc92b6c774a3ed0e7ce6
Vivek Joshi <vivekjoshi15@gmail.com>
2018-11-23 00:49:28 +0530

    change

--- src/app/common/modals/modal.component.ts
+++ src/app/common/modals/modal.component.ts
@@ -12,7 +12,7 @@ import { ModalService } from './modal.service';
 
 export class ModalComponent implements OnInit, OnDestroy {
     @Input() id: string;
-    private element: jQuery;
+    private element: JQuery;
 
     constructor(private modalService: ModalService, private el: ElementRef) {
         this.element = $(el.nativeElement);
